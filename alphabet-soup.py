1315a3015d02d78e6b2f7e0b37450e01529c9d3b
enzocazenave <encaze24@hotmail.com>
2022-10-16 21:04:38 -0300

    Add script

---
+++ alphabet-soup.py
@@ -0,0 +1,6 @@
+# Funcion para mostrar la sopa de letras | ARGS: { 1: 'Matriz de sopa de letras' }
+def mostrar_sopa(sopa):
+    for fila in sopa:
+        for columna in fila:
+            print(columna, end=" ")
+        print()
